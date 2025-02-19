dc2925841e31b85f331b4acda2ef8ce71a912ac1
Alungoo-T <alungoo976@gmail.com>
2025-02-18 22:07:57 -0500

    added new proj

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" font-extrabold text-7xl flex justify-center items-center h-[50vh]">
+      Ssup
+    </div>
+  );
+};
+
+export default Home;
